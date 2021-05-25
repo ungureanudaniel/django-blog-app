50e3e860c0ca41776ec42780306bdd2564f0fde7
ungureanudaniel <ungureanu.daniel86@gmail.com>
2021-05-25 23:42:02 +0300

    Social django error fixed

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -71,20 +71,10 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'social_django.context_processors.backends',
-                'social_django.context_processors.login_redirect',
             ],
         },
     },
 ]
-
-AUTHENTICATION_BACKENDS = (
-    'social_core.backends.facebook.FacebookOAuth2',
-    'social_core.backends.twitter.TwitterOAuth',
-    'social_core.backends.google.GooglePlusAuth',
-
-    'django.contrib.auth.backends.ModelBackend',
-)
 
 WSGI_APPLICATION = "blogproject.wsgi.application"
 
