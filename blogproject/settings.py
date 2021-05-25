06cd2d18552fa3d03e57026d236a86bb8d365761
ungureanudaniel <ungureanu.daniel86@gmail.com>
2021-05-25 23:22:23 +0300

    Added new domain to allowed hosts

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'malluldeganduri.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'www.malluldeganduri.com', 'malluldeganduri.com']
 
 # Application definition
 
